22ec5fa7e1cd10d8f2b0de1b65eeff12105b5b2f
reshan <reshanredomni@gmail.com>
2019-03-06 14:21:53 +0530

    template setup

--- adminpanelvue/resources/views/auth/login.blade.php
+++ adminpanelvue/resources/views/auth/login.blade.php
@@ -1,73 +1,31 @@
-@extends('layouts.app')
-
+@extends('layouts.app') 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
+<div class="d-flex align-items-center justify-content-center bg-br-primary ht-100v">
+    <div class="login-wrapper wd-300 wd-xs-350 pd-25 pd-xs-40 bg-white rounded shadow-base">
+        <div class="signin-logo tx-center tx-28 tx-bold tx-inverse"><span class="tx-normal"></span> Login <span class="tx-normal"></span></div>
+        <div class="tx-center mg-b-60">Admin Login Lowe's</div>
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="form-group">
+                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>                @if ($errors->has('email'))
+                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                                    </span> @endif
             </div>
-        </div>
+            <!-- form-group -->
+            <div class="form-group">
+                <input id="password" type="password" class="form-control" name="password" required> @if ($errors->has('password'))
+                <span class="invalid-feedback" role="alert">
+                <strong>{{ $errors->first('password') }}</strong>
+            </span> @endif
+            </div>
+            <!-- form-group -->
+            <button type="submit" class="btn btn-info btn-block">Sign In</button>
+        </form>
     </div>
+    <!-- login-wrapper -->
 </div>
+<!-- d-flex -->
+
+
 @endsection
--- adminpanelvue/resources/views/layouts/app.blade.php
+++ adminpanelvue/resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,71 +11,274 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+    <link href="./admin/lib/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="./admin/lib/Ionicons/css/ionicons.css" rel="stylesheet">
+    <link href="./admin/lib/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
+    <link href="./admin/lib/jquery-switchbutton/jquery.switchButton.css" rel="stylesheet">
+    <link href="./admin/lib/rickshaw/rickshaw.min.css" rel="stylesheet">
+    <link href="./admin/lib/chartist/chartist.css" rel="stylesheet">
+
+    <!-- Bracket CSS -->
+    <link rel="stylesheet" href="./admin/css/bracket.css">
 </head>
+
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    {{-- <div id="app">
+        <div class="container"> --}}
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+            <!-- Right Side Of Navbar -->
 
-                    </ul>
+            <!-- Authentication Links -->
+            @guest @else {{--
+            <ul class="navbar-nav ml-auto">
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            </ul> --}}
+            <div class="br-logo"><a href=""><span>[</span>bracket<span>]</span></a></div>
+            <div class="br-sideleft overflow-y-auto">
+              <label class="sidebar-label pd-x-15 mg-t-20">Navigation</label>
+              <div class="br-sideleft-menu">
+                <a href="index.html" class="br-menu-link active">
+                  <div class="br-menu-item">
+                    <i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>
+                    <span class="menu-item-label">Dashboard</span>
+                  </div><!-- menu-item -->
+                </a><!-- br-menu-link -->
+                <a href="mailbox.html" class="br-menu-link">
+                  <div class="br-menu-item">
+                    <i class="menu-item-icon icon ion-ios-email-outline tx-24"></i>
+                    <span class="menu-item-label">Mailbox</span>
+                  </div><!-- menu-item -->
+                </a><!-- br-menu-link -->
+                <a href="card-dashboard.html" class="br-menu-link">
+                  <div class="br-menu-item">
+                    <i class="menu-item-icon icon ion-ios-photos-outline tx-20"></i>
+                    <span class="menu-item-label">Cards &amp; Widgets</span>
+                  </div><!-- menu-item -->
+                </a><!-- br-menu-link -->
+                <a href="#" class="br-menu-link">
+                  <div class="br-menu-item">
+                    <i class="menu-item-icon icon ion-ios-filing-outline tx-24"></i>
+                    <span class="menu-item-label">UI Elements</span>
+                    <i class="menu-item-arrow fa fa-angle-down"></i>
+                  </div><!-- menu-item -->
+                </a><!-- br-menu-link -->
+                <ul class="br-menu-sub nav flex-column">
+                  <li class="nav-item"><a href="accordion.html" class="nav-link">Accordion</a></li>
+                  <li class="nav-item"><a href="alerts.html" class="nav-link">Alerts</a></li>
+                  <li class="nav-item"><a href="buttons.html" class="nav-link">Buttons</a></li>
+                  <li class="nav-item"><a href="cards.html" class="nav-link">Cards</a></li>
+                  <li class="nav-item"><a href="icons.html" class="nav-link">Icons</a></li>
+                  <li class="nav-item"><a href="modal.html" class="nav-link">Modal</a></li>
+                  <li class="nav-item"><a href="pagination.html" class="nav-link">Pagination</a></li>
+                  <li class="nav-item"><a href="popups.html" class="nav-link">Tooltip &amp; Popover</a></li>
+                  <li class="nav-item"><a href="progress.html" class="nav-link">Progress</a></li>
+                  <li class="nav-item"><a href="spinners.html" class="nav-link">Spinners</a></li>
+                  <li class="nav-item"><a href="typography.html" class="nav-link">Typography</a></li>
+                </ul>
+                <a href="#" class="br-menu-link">
+                  <div class="br-menu-item">
+                    <i class="menu-item-icon ion-ios-redo-outline tx-24"></i>
+                    <span class="menu-item-label">Navigation</span>
+                    <i class="menu-item-arrow fa fa-angle-down"></i>
+                  </div><!-- menu-item -->
+                </a><!-- br-menu-link -->
+                <ul class="br-menu-sub nav flex-column">
+                  <li class="nav-item"><a href="navigation.html" class="nav-link">Basic Nav</a></li>
+                  <li class="nav-item"><a href="navigation-layouts.html" class="nav-link">Nav Layouts</a></li>
+                </ul>
+                <a href="#" class="br-menu-link">
+                  <div class="br-menu-item">
+                    <i class="menu-item-icon ion-ios-pie-outline tx-20"></i>
+                    <span class="menu-item-label">Charts</span>
+                    <i class="menu-item-arrow fa fa-angle-down"></i>
+                  </div><!-- menu-item -->
+                </a><!-- br-menu-link -->
+                <ul class="br-menu-sub nav flex-column">
+                  <li class="nav-item"><a href="chart-morris.html" class="nav-link">Morris Charts</a></li>
+                  <li class="nav-item"><a href="chart-flot.html" class="nav-link">Flot Charts</a></li>
+                  <li class="nav-item"><a href="chart-chartjs.html" class="nav-link">Chart JS</a></li>
+                  <li class="nav-item"><a href="chart-rickshaw.html" class="nav-link">Rickshaw</a></li>
+                  <li class="nav-item"><a href="chart-chartist.html" class="nav-link">Chartist</a></li>
+                  <li class="nav-item"><a href="chart-sparkline.html" class="nav-link">Sparkline</a></li>
+                  <li class="nav-item"><a href="chart-peity.html" class="nav-link">Peity</a></li>
+                </ul>
+                <a href="#" class="br-menu-link">
+                  <div class="br-menu-item">
+                    <i class="menu-item-icon icon ion-ios-gear-outline tx-24"></i>
+                    <span class="menu-item-label">Forms</span>
+                    <i class="menu-item-arrow fa fa-angle-down"></i>
+                  </div><!-- menu-item -->
+                </a><!-- br-menu-link -->
+                <ul class="br-menu-sub nav flex-column">
+                  <li class="nav-item"><a href="form-elements.html" class="nav-link">Form Elements</a></li>
+                  <li class="nav-item"><a href="form-layouts.html" class="nav-link">Form Layouts</a></li>
+                  <li class="nav-item"><a href="form-validation.html" class="nav-link">Form Validation</a></li>
+                  <li class="nav-item"><a href="form-wizards.html" class="nav-link">Form Wizards</a></li>
+                  <li class="nav-item"><a href="form-editor-code.html" class="nav-link">Code Editor</a></li>
+                  <li class="nav-item"><a href="form-editor-text.html" class="nav-link">Text Editor</a></li>
+                </ul>
+                <a href="#" class="br-menu-link">
+                  <div class="br-menu-item">
+                    <i class="menu-item-icon icon ion-ios-bookmarks-outline tx-20"></i>
+                    <span class="menu-item-label">Tables</span>
+                    <i class="menu-item-arrow fa fa-angle-down"></i>
+                  </div><!-- menu-item -->
+                </a><!-- br-menu-link -->
+                <ul class="br-menu-sub nav flex-column">
+                  <li class="nav-item"><a href="table-basic.html" class="nav-link">Basic Table</a></li>
+                  <li class="nav-item"><a href="table-datatable.html" class="nav-link">Data Table</a></li>
+                </ul>
+                <a href="#" class="br-menu-link">
+                  <div class="br-menu-item">
+                    <i class="menu-item-icon icon ion-ios-navigate-outline tx-24"></i>
+                    <span class="menu-item-label">Maps</span>
+                    <i class="menu-item-arrow fa fa-angle-down"></i>
+                  </div><!-- menu-item -->
+                </a><!-- br-menu-link -->
+                <ul class="br-menu-sub nav flex-column">
+                  <li class="nav-item"><a href="map-google.html" class="nav-link">Google Maps</a></li>
+                  <li class="nav-item"><a href="map-leaflet.html" class="nav-link">Leaflet Maps</a></li>
+                  <li class="nav-item"><a href="map-vector.html" class="nav-link">Vector Maps</a></li>
+                </ul>
+                <a href="#" class="br-menu-link">
+                  <div class="br-menu-item">
+                    <i class="menu-item-icon icon ion-ios-briefcase-outline tx-22"></i>
+                    <span class="menu-item-label">Utilities</span>
+                    <i class="menu-item-arrow fa fa-angle-down"></i>
+                  </div><!-- menu-item -->
+                </a><!-- br-menu-link -->
+                <ul class="br-menu-sub nav flex-column">
+                  <li class="nav-item"><a href="background.html" class="nav-link">Background</a></li>
+                  <li class="nav-item"><a href="border.html" class="nav-link">Border</a></li>
+                  <li class="nav-item"><a href="height.html" class="nav-link">Height</a></li>
+                  <li class="nav-item"><a href="margin.html" class="nav-link">Margin</a></li>
+                  <li class="nav-item"><a href="padding.html" class="nav-link">Padding</a></li>
+                  <li class="nav-item"><a href="position.html" class="nav-link">Position</a></li>
+                  <li class="nav-item"><a href="typography-util.html" class="nav-link">Typography</a></li>
+                  <li class="nav-item"><a href="width.html" class="nav-link">Width</a></li>
+                </ul>
+                <a href="pages.html" class="br-menu-link">
+                  <div class="br-menu-item">
+                    <i class="menu-item-icon icon ion-ios-paper-outline tx-22"></i>
+                    <span class="menu-item-label">Apps &amp; Pages</span>
+                  </div><!-- menu-item -->
+                </a><!-- br-menu-link -->
+                <a href="layouts.html" class="br-menu-link">
+                  <div class="br-menu-item">
+                    <i class="menu-item-icon icon ion-ios-book-outline tx-22"></i>
+                    <span class="menu-item-label">Layouts</span>
+                  </div><!-- menu-item -->
+                </a><!-- br-menu-link -->
+                <a href="sitemap.html" class="br-menu-link">
+                  <div class="br-menu-item">
+                    <i class="menu-item-icon icon ion-ios-list-outline tx-22"></i>
+                    <span class="menu-item-label">Sitemap</span>
+                  </div><!-- menu-item -->
+                </a><!-- br-menu-link -->
+              </div><!-- br-sideleft-menu -->
+        
+              <label class="sidebar-label pd-x-15 mg-t-25 mg-b-20 tx-info op-9">Information Summary</label>
+        
+              <br>
+            </div><!-- br-sideleft -->
+            <!-- ########## END: LEFT PANEL ########## -->
+        
+            <!-- ########## START: HEAD PANEL ########## -->
+            <div class="br-header">
+              <div class="br-header-left">
+                <div class="navicon-left hidden-md-down"><a id="btnLeftMenu" href=""><i class="icon ion-navicon-round"></i></a></div>
+                <div class="navicon-left hidden-lg-up"><a id="btnLeftMenuMobile" href=""><i class="icon ion-navicon-round"></i></a></div>
+                
+              </div><!-- br-header-left -->
+              <div class="br-header-right">
+                <nav class="nav">
+                 
+                  <div class="dropdown">
+                    <a href="" class="nav-link nav-link-profile" data-toggle="dropdown">
+                      <span class="logged-name hidden-md-down">Katherine</span>
+                      <img src="http://via.placeholder.com/64x64" class="wd-32 rounded-circle" alt="">
+                      <span class="square-10 bg-success"></span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-header wd-200">
+                      <ul class="list-unstyled user-profile-nav">
+                        <li><a href=""><i class="icon ion-ios-person"></i> Edit Profile</a></li>
+                        <li><a href=""><i class="icon ion-ios-gear"></i> Settings</a></li>
+                        <li><a href=""><i class="icon ion-ios-download"></i> Downloads</a></li>
+                        <li><a href=""><i class="icon ion-ios-star"></i> Favorites</a></li>
+                        <li><a href=""><i class="icon ion-ios-folder"></i> Collections</a></li>
+                        <li><a href=""><i class="icon ion-power"></i> Sign Out</a></li>
+                      </ul>
+                    </div><!-- dropdown-menu -->
+                  </div><!-- dropdown -->
+                </nav>
+              </div><!-- br-header-right -->
+            </div><!-- br-header -->
+            <!-- ########## END: HEAD PANEL ########## -->
+        
+        
+            <!-- ########## START: MAIN PANEL ########## -->
+            <div class="br-mainpanel">
+             
+              </div>
+             @endguest
 
+        {{-- </div> --}}
         <main class="py-4">
             @yield('content')
         </main>
-    </div>
+        <footer class="br-footer">
+            <div class="footer-left">
+                <div class="mg-b-2">Copyright &copy; 2017. Bracket. All Rights Reserved.</div>
+                <div>Attentively and carefully made by ThemePixels.</div>
+            </div>
+            <div class="footer-right d-flex align-items-center">
+                <span class="tx-uppercase mg-r-10">Share:</span>
+                <a target="_blank" class="pd-x-5" href="https://www.facebook.com/sharer/sharer.php?u=http%3A//themepixels.me/bracket/intro"><i class="fa fa-facebook tx-20"></i></a>
+                <a target="_blank" class="pd-x-5" href="https://twitter.com/home?status=Bracket,%20your%20best%20choice%20for%20premium%20quality%20admin%20template%20from%20Bootstrap.%20Get%20it%20now%20at%20http%3A//themepixels.me/bracket/intro"><i class="fa fa-twitter tx-20"></i></a>
+            </div>
+        </footer>
+    {{-- </div> --}}
+    <script src="./admin/lib/jquery/jquery.js"></script>
+    <script src="./admin/lib/popper.js/popper.js"></script>
+    <script src="./admin/lib/bootstrap/bootstrap.js"></script>
+    <script src="./admin/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.js"></script>
+    <script src="./admin/lib/moment/moment.js"></script>
+    <script src="./admin/lib/jquery-ui/jquery-ui.js"></script>
+    <script src="./admin/lib/jquery-switchbutton/jquery.switchButton.js"></script>
+    <script src="./admin/lib/peity/jquery.peity.js"></script>
+    <script src="./admin/lib/chartist/chartist.js"></script>
+    <script src="./admin/lib/jquery.sparkline.bower/jquery.sparkline.min.js"></script>
+    <script src="./admin/lib/d3/d3.js"></script>
+    <script src="./admin/lib/rickshaw/rickshaw.min.js"></script>
+    <script src="./admin/js/bracket.js"></script>
+    <script src="./admin/js/ResizeSensor.js"></script>
+    <script src="./admin/js/dashboard.js"></script>
+    @yield('js')
 </body>
+
 </html>
