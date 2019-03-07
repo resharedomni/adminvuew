c880fd6b2eec90ce04dafc40e9da8973496b6bf1
reshan <reshanredomni@gmail.com>
2019-03-07 17:15:33 +0530

    user module

--- adminpanelvue/resources/views/layouts/app.blade.php
+++ adminpanelvue/resources/views/layouts/app.blade.php
@@ -63,14 +63,14 @@
             <div class="br-sideleft overflow-y-auto">
               <label class="sidebar-label pd-x-15 mg-t-20">Navigation</label>
               <div class="br-sideleft-menu">
-                <a href="index.html" class="br-menu-link active">
+                <a href="{{ url('/home') }}" class="br-menu-link active">
                   <div class="br-menu-item">
                     <i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>
                     <span class="menu-item-label">Dashboard</span>
                   </div><!-- menu-item -->
                 </a><!-- br-menu-link -->
                 
-                <a href="card-dashboard.html" class="br-menu-link">
+                <a href="{{ url('/user') }}" class="br-menu-link">
                   <div class="br-menu-item">
                     <i class="menu-item-icon icon ion-ios-photos-outline tx-20"></i>
                     <span class="menu-item-label">User Management</span>
